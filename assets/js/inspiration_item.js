c6503423905507bf6b7b94651d7e45805833048d
Olivia <oliviachang29@gmail.com>
2020-04-22 15:10:49 -0700

    add actual instagram feed and favicon

---
+++ assets/js/inspiration_item.js
@@ -0,0 +1,1 @@
+$('img').trigger('unveil');
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,31 +11,11 @@ function markCurrentLink(target) {
 function unveilImages(selector) {
   selector.unveil({
       offset: 200,
-      // throttle: 200,
       placeholder: '/assets/images/lazy.jpg',
-      // breakpoints: [
-      //     {
-      //         minWidth: 768,
-      //         attribute: 'data-src-md'
-      //     },
-      //     {
-      //         minWidth: 1200,
-      //         attribute: 'data-src-lg'
-      //     }
-      // ],
       debug: true
-  }).on('loading.unveil', function () {
-      // console.log('Unveiling', this);
   }).on('loaded.unveil', function () {
-      // console.log('Unveiled', this);
       this.style.opacity = 1;
   });
-
-  // selector.unveil(200, function() {
-  //   $(this).on('load', function() {
-  //     this.style.opacity = 1;
-  //   });
-  // });
 }
 
 function initJS() {
@@ -44,17 +24,9 @@ function initJS() {
     var feed = new Instafeed({
         limit: 3,
         accessToken: InstagramToken,
-        debug: true,
-        template: '<a class="col-md-4 instagram-post" href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>'
+        template: '<a class="instagram-post" href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>'
     });
     feed.run();
 }
-// executes this stuff before load
-// $(function() {
-//     const swup = new Swup();
-//     swup.on('contentReplaced', function() {
-//         initJS()
-//     });
-// });
 
 initJS()
